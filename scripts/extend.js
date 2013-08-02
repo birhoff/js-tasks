11993880c7f9c3b6e3dddbed028209f213872439
birhoff <birhoff@gmail.com>
2013-08-02 23:32:51 +0300

    Update extend.js

--- scripts/extend.js
+++ scripts/extend.js
@@ -13,39 +13,20 @@ function extend(P, px, sx){
 }
 
 function mixin(dst, src){
-
-    // tobj - вспомогательный объект для фильтрации свойств,
-
-    // которые есть у объекта Object и его прототипа
-
     var tobj = {}
 
     for(var x in src){
-
-        // копируем в dst свойства src, кроме тех, которые унаследованы от Object
-
         if((typeof tobj[x] == "undefined") || (tobj[x] != src[x])){
-
             dst[x] = src[x];
-
         }
-
     }
 
-    // В IE пользовательский метод toString отсутствует в for..in
-
     if(document.all && !document.isOpera){
-
         var p = src.toString;
 
         if(typeof p == "function" && p != dst.toString && p != tobj.toString &&
-
          p != "\nfunction toString() {\n    [native code]\n}\n"){
-
             dst.toString = src.toString;
-
         }
-
     }
-
 }
