6529e6a196d3d6c028b99a68a2f9790fef314f57
birhoff <birhoff@gmail.com>
2013-08-04 16:14:30 +0300

    Update extend.js

--- scripts/extend.js
+++ scripts/extend.js
@@ -12,21 +12,39 @@ function extend(P, px, sx){
     }());
 }
 
-function mixin(dst, src){
-    var tobj = {}
+var mixin = (function () {
+    // test IE prototype for/in error
+    var hasIEError = true;
+    for (var p in {toString: null}) hasIEError = false;
 
-    for(var x in src){
-        if((typeof tobj[x] == "undefined") || (tobj[x] != src[x])){
-            dst[x] = src[x];
+    if (!hasIEError) {
+        return function extend(dst) {
+            for (var i = 0; i < arguments.length; i++) {
+                var source = arguments[i];
+                for (var prop in source) {
+                    dst[prop] = source[prop];
+                }
+            }
+            return dst;
+        }
+    } else {
+        var prototypeProps = ["toString", "valueOf", "constructor", "hasOwnProperty", "toLocaleString", "isPrototypeOf", "propertyIsEnumerable"];
+        return function extend(dst) {
+            for (var i = 0; i < arguments.length; i++) {
+                var source = arguments[i];
+
+                for (var prop in source) {
+                    dst[prop] = source[prop];
+                }
+
+                for (var j = 0; j < prototypeProps.length; j++) {
+                    prop = prototypeProps[j];
+                    if (source.hasOwnProperty(prop)) {
+                        dst[prop] = source[prop];
+                    }
+                }
+            }
+            return dst;
         }
     }
-
-    if(document.all && !document.isOpera){
-        var p = src.toString;
-
-        if(typeof p == "function" && p != dst.toString && p != tobj.toString &&
-         p != "\nfunction toString() {\n    [native code]\n}\n"){
-            dst.toString = src.toString;
-        }
-    }
-}
+}());
